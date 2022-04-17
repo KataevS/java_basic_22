b6633020bda05720678d5c3a59c75dd324e7bd6d
KataevS <sergey.v.kataev@gmail.com>
2022-04-17 19:44:25 +0300

    new file Homework.java

---
+++ Homework.java
@@ -0,0 +1,12 @@
+import java.util.Base64;
+import java.util.Scanner;
+public class Homework{
+	
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your full name");
+        String fullName = scanner.nextLine();
+        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
+        System.out.println("Program result: " + encodedFullName);
+    }
+}
